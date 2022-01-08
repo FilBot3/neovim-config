569c1b1b9363e237174b26adfa11850b51a6c88f
Phillip Dudley <Predatorian3@gmail.com>
2022-01-07 19:26:47 -0600

    Added ansiblels and made lsp-config float diagnostics.

--- lua/lsp-configs.lua
+++ lua/lsp-configs.lua
@@ -2,7 +2,11 @@
 -- Requires NodeJS and npm to be available in your PATH.
 -- @see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
+-- Make the Diagnostics show up in a floating window.
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
+require'lspconfig'.ansiblels.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.eslint.setup{}
