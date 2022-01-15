d8c1fef4b0b2843260c23fda36909fe051ddf1d1
Phillip Dudley <Predatorian3@gmail.com>
2022-01-15 14:26:33 -0600

    Figured out how to override the LSP CMD location. However, just commenting it out for now. Need to get the rest installed via this installer.

--- lua/lsp-configs.lua
+++ lua/lsp-configs.lua
@@ -8,6 +8,9 @@ vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {f
 
 require'lspconfig'.ansiblels.setup{}
 require'lspconfig'.bashls.setup{}
+--require'lspconfig'.bashls.setup{
+--  cmd = { vim.fn.stdpath('data') .. "/lsp_servers/bash/node_modules/.bin/bash-language-server", "start" }
+--}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.eslint.setup{}
 require'lspconfig'.jsonls.setup{}
