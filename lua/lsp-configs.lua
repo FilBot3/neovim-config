1b073d716be44cd30bd317a0e51627f6ba3ed60a
Phillip Dudley <Predatorian3@gmail.com>
2022-02-09 12:02:15 -0600

    Updated language server locations to look in local dir since using lsp-insatller

--- lua/lsp-configs.lua
+++ lua/lsp-configs.lua
@@ -10,10 +10,21 @@ require'lspconfig'.ansiblels.setup{}
 require'lspconfig'.bashls.setup{
   cmd = { vim.fn.stdpath('data') .. "/lsp_servers/bash/node_modules/.bin/bash-language-server", "start" }
 }
-require'lspconfig'.dockerls.setup{}
-require'lspconfig'.eslint.setup{}
-require'lspconfig'.jsonls.setup{}
-require'lspconfig'.terraformls.setup{}
+require'lspconfig'.dockerls.setup{
+  cmd = { vim.fn.stdpath('data') .. "/lsp_servers/dockerfile/node_modules/.bin/docker-langserver", "start" }
+}
+require'lspconfig'.eslint.setup{
+  cmd = { vim.fn.stdpath('data') .. "/lsp_servers/vscode-eslint/node_modules/.bin/vscode-eslint-language-server", "start" }
+}
+require'lspconfig'.jsonls.setup{
+  cmd = { vim.fn.stdpath('data') .. "/lsp_servers/jsonls/node_modules/.bin/vscode-json-language-server", "start" }
+}
+require'lspconfig'.pyright.setup{
+  cmd = { vim.fn.stdpath('data') .. "/lsp_servers/python/node_modules/.bin/pyright-langserver", "--stdio" }
+}
+require'lspconfig'.terraformls.setup{
+  cmd = { vim.fn.stdpath('data') .. "/lsp_servers/terraform/terraform-ls/terraform-ls", "serve" }
+}
 require'lspconfig'.yamlls.setup{
   cmd = { vim.fn.stdpath('data') .. "/lsp_servers/yaml/node_modules/.bin/yaml-language-server", "--stdio" }
 }
